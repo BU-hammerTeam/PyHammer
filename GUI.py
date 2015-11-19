49d9ca147f22db73ca8670e953640d3bf0dbd773
aurorayk <aurorak@bu.edu>
2015-11-19 11:16:01 -0500

    Create GUI.py

---
+++ GUI.py
@@ -0,0 +1,10 @@
+"""
+Description: 
+This makes the GUI. Someone who knows more about what goes in the GUI can update the docstring. 
+
+Input: 
+
+Output: 
+Eyeout.tbl - this contains the spectrum filename, the inital guess + metallicity, by-eye spectral typing results + metallicity
+"""
+
