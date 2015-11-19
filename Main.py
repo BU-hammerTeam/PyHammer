0b11abd367423f4cad508655eeb643efc30d5bc7
aurorayk <aurorak@bu.edu>
2015-11-19 10:54:41 -0500

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,31 @@
+
+"""
+Main(spectraFilename)
+
+Description: 
+This is the main portion of the code. It calls the other functions used. It's main function is to: 
+call the Spectrum class, do the radial velocity measurements, 
+cross correlate the lines to get the inital spectral type and metallicity guess, bring up the GUI. 
+
+Input: 
+spectraFilename - a list of the filenames of each of the spectra to be classified (one per line), 
+and the corresponding filetype (i.e., sdssfits, fits, txt, etc.)
+
+Output: 
+autoSpTResults.tbl - list of the spectra with the results of the auto spectral typing and metallicity results. 
+"""
+
+#function that does the radial velocity code
+def radialVelocity(): 
+    #Dylan's code goes here
+
+
+#Check for the correct path
+#CAN TRANSLATE
+
+#Check if user wants to run the autospectral typing
+#CAN TRANSLATE
+
+#Check if user wants a S/N cut
+#CAN TRANSLATE
+
