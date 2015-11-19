0989059ed1af8a768af4f3d312dc3156b62fba64
aurorayk <aurorak@bu.edu>
2015-11-19 11:31:32 -0500

    Create Spectrum.py

---
+++ Spectrum.py
@@ -0,0 +1,34 @@
+"""
+Spectrum(fileName)
+
+Description: 
+This is a class that contains all the information about the spectrum. It will read in the filename, then measure the lines
+of the spectrum. 
+
+Input: 
+fileName - the filename of the spectrum 
+
+Output: 
+lineIndices - the measured line indices
+"""
+
+#create the spectrum class
+class Spectrum(object): 
+    
+    #pass in the filename to the spectrum
+    def __init__(self, fileName): 
+        self.fileName = fileName
+        
+        #open the file
+        #THIS NEEDS TO BE IMPROVED FROM THE ORIGINAL HAMMER
+    
+    #Make a function that measures the lines indices
+    def measureLines(): 
+        #CAN USE THE SAME LINES AS HAMMER, BUT IMPROVE THE CODE LAYOUT
+        
+        #Figure out which lines are important for the star (that ratio part)
+        #CAN TRANSLATE FROM HAMMER
+        
+        return lineIndices
+    
+        
