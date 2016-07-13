6010c53b98700475f455de8d5ef8649a9952c677
Connor Robinson <Connor@wireless1x-155-41-102-88.bu.edu>
2016-07-13 11:45:12 -0400

    Added interpolation function

--- spectrum.py
+++ spectrum.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import interpolate
 from astropy.io import fits
 
 class Spectrum(object):
@@ -75,21 +76,79 @@ class Spectrum(object):
         is already in vacuum or air.
         Sloan, princeton are already in air but most other spectra are not. 
         """
-
         print('Not implemented')
 
-    def _interpOntoGrid(self):
+    def _interpOntoGrid(self, log = True, tfile = 'default', tlog = 'True'):
+        import pdb
         """
+        
+        Description:
         A method to interpolate the wavelength, flux and noise
         onto a logarithmic wavelength spacing. The observed spectrum
         and the templates need to be consistent with one another.
         
+        Input:
+        
+        log   - [Boolean Keyword] Set to true if wavelength for spectra is in log space.
+                Default is True.
+        
+        tfile - [Optional] The name + path to a file containing a sample
+                   template spectral different than the defaults provided.
+        
+        tlog  - [Boolean Keyword] Set to true if the wavelength for template spectra
+                is in log space. Default is True (Default spectra are in log).
+        
+        Output:
+        A spectra interpolated onto a logarithmic grid.
+        
+        Notes:
+        All wavelengths should be in angstroms or log angstroms with the correct log/tlog tag.
+        
+        The wavelength information is taken from sample_template.fits in the
+        resources folder. If using a different spectral library than the default
+        provided with this code, can change the keyword.
+        
+        If the custom template is NOT in log lambda space, the column containing the wavelength 
+        information MUST be labeled 'Lambda'
+        
+        The interpolation is done using cubic spline interpolation, which
+        should provide accurate interpolation without a large investment of
+        computing time. (see arXiv:1301.4843[astro-ph.SR])
+        
         [Aurora] need some if statement to check to see if the spectrum 
         is in equal RV space. If it is not, we will want to choose the 
         blue (lower RV) end to interpolate onto
+        
+        This comment is related to the resolution of the bins, equal sized bins
+        in log space are not equal in normal space.
+        
+        The default template is linearly binned in velocity space -> log(lambda) space
+        
         """
-
-        print('Not Implemented')
+        #Grab an sample template to get wavelengths for interpolation
+        if tfile == 'default':
+            template = fits.open('resources/sample_template.fits')
+        else:
+            template = fits.open(tfile)
+        
+        #Transform the wavelength grid from the sample to a linear grid if it's in log space
+        if tlog == True:
+            grid = 10**template[1].data['LogLam']
+        elif tlog == False:
+            grid = template[1].data['Lambda']
+            
+        #Interpolate onto the logarithmic grid with a cubic spline fit
+        if log == True:
+            func = interpolate.interp1d(10**self.wavelength, self.flux, kind='cubic', bounds_error = False)
+            noisefunc = interpolate.interp1d(10**self.wavelength, self.noise, kind = 'cubic', bounds_error = False)
+        elif log == False:
+            func = interpolate.interp1d(self.wavelength, self.flux, kind='cubic', bounds_error = False)
+            noisefunc = interpolate.interp1d(self.wavelength, self.noise, kind='cubic', bounds_error = False)
+        
+        #Want all the space to be in log lambda space in the end, so make sure they are if they are not
+        #Convert the wavelength grid to log lambda
+        self.wavelength = np.log10(grid)
+        self.flux = func(grid)
 
     def measureLines(self):
         """
