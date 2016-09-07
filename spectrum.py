add74fdb7454c50d86994f89fad13bb8ea9b9ff6
zephyr5050 <zephyr5050@users.noreply.github.com>
2016-09-07 17:30:49 -0400

    Changing removing fits from spectrum data type

--- spectrum.py
+++ spectrum.py
@@ -171,7 +171,7 @@ class Spectrum(object):
                 errorMessage = 'Unable to use ' + filename + '.\n' + str(e)
                 return False, errorMessage
             
-        elif (filetype.lower() == 'sdssdr7fits'):
+        elif (filetype.lower() == 'sdssdr7'):
             # Implement reading a sdss EDR through DR8 fits file
             try:
                 with warnings.catch_warnings():
@@ -192,7 +192,7 @@ class Spectrum(object):
                 errorMessage = 'Unable to use ' + filename + '.\n' + str(e)
                 return False, errorMessage
             
-        elif (filetype.lower() == 'sdssdr12fits'): 
+        elif (filetype.lower() == 'sdssdr12'): 
             # Implement reading a sdss DR9 through DR12 fits file
             try:
                 with warnings.catch_warnings():
